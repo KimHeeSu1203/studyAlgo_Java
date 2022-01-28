bf2ff63a0dac100f74b313129adb380c92263602
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-28 23:35:33 +0900

    220128 / 입출력과 사칙연산 / 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/boj18108.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class boj18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        sc.close();
+        System.out.println(y-543);
+    }
+}
