368bf88376f6a4c6ec36c521a9aeeced7b7b331f
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-29 14:49:52 +0900

    220129 / if문 / A+B - 7

---
+++ src/boj11021.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class boj11021 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        for (int i = 1; i <= num ; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+i+": "+ (a+b));
+        }
+        sc.close();
+    }
+}
