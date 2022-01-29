66da90ce80c0fb1d95873d473406e1b96589de7d
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-29 15:18:33 +0900

    220129 / for문 / X보다 작은 수

---
+++ src/boj10871.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class boj10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            if(a<X){
+                System.out.print(a+" ");
+            }
+
+        }
+
+    }
+}
