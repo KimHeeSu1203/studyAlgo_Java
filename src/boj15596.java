d7f9201016b709485c94b264374aba060f9c300a
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-30 21:47:01 +0900

    220130 / 함수 / 정수 N개의 합

---
+++ src/boj15596.java
@@ -0,0 +1,14 @@
+public class boj15596 {
+    public static void main(String[] args) {
+    }
+
+    public class Test {
+        long sum(int[] a) {
+            long ans = 0;
+            for (int i = 0; i < a.length; i++) {
+                ans += a[i];
+            }
+            return ans;
+        }
+    }
+}
